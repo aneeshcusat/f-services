a265ea6c85dd556a15969f279384e26f8ef47fe6
Aneeshkumar <Aneeshkumar@Aneeshkumar-PC>
2016-05-31 15:01:23 +0530

    initial commit

---
+++ Base/src/main/com/chathan/famstack/manager/profile/UserProfileManager.java
@@ -0,0 +1,17 @@
+package com.chathan.famstack.manager.profile;
+
+public class UserProfileManager {
+
+	public String registerUser() {
+		return "user registered";
+	}
+
+	public String login() {
+		return "logged in";
+	}
+
+	public String getUserDetails() {
+		return "user details";
+	}
+
+}
